d92bb382d3a90ae8d5ef5c2fb042c386855b5845
Semyon Esaev <semen.esaev@yandex.ru>
2023-04-20 01:44:29 +0300

    main: init project

---
+++ src/main/java/ru/skqwk/zettedelebackend/ZettedeleBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.skqwk.zettedelebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZettedeleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZettedeleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skqwk/zettedelebackend/ZettedeleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skqwk.zettedelebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZettedeleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
